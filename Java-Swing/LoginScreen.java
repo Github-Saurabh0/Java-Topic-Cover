8d7653332944bfa22e19416ca710cf9929e610d4
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-05-29 17:09:20 +0530

    Java topic-wise learning programs

--- Java-Swing/LoginScreen.java
+++ Java-Swing/LoginScreen.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-
+import java.awt.event.*;
 class LoginScreen extends JFrame
 {
 JLabel l1,l2,l3,l4;
@@ -21,7 +21,7 @@ l2 = new JLabel("USERNAME");
 l3 = new JLabel("PASSWORD");
 l4 = new JLabel();
 t1 = new JTextField();
-t2 = new JTextField();
+t2 = new JPasswordField();
 b1 = new JButton("Login");
 b2 = new JButton("Clear");
 setLayout(null);
@@ -36,12 +36,12 @@ add(b2);
 l1.setBounds(100, 50, 300, 30);
 l2.setBounds(100, 200, 100, 30);
 l3.setBounds(100, 350, 100, 30);
-l4.setBounds(100, 550, 100, 30);
+l4.setBounds(100, 550, 200, 30);
 t1.setBounds(350, 200, 100, 30);
 t2.setBounds(350, 350, 100, 30);
 b1.setBounds(200, 450, 100, 30);
 b2.setBounds(400, 450, 100, 30);
-
+b1.addActionListener(new Log());
 }
 
 public static void main(String [] args)
@@ -52,6 +52,21 @@ s1.setVisible(true);
 s1.setSize(700,700);
 s1.setComponents();
 s1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+}
+class Log implements ActionListener
+{
+public void actionPerformed(ActionEvent e1)	
+{
+String s1 = t1.getText();
+String s2 = t2.getText();
+if(s1.equals("Saurabhh") && s2.equals("Java"))
+{
+	l4.setText("Login Successfull..");
+}		
+else
+{
+	l4.setText("Login UnSuccessfull..");
+}
+}
 }
 }
