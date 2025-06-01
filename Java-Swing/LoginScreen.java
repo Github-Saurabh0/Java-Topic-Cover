0feb2ba32f947e389b7a56964c70057bef114ba3
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-01 21:25:05 +0530

    Mouse Event on Username, Font Add, Button Background, Cursor Add on Button

--- Java-Swing/LoginScreen.java
+++ Java-Swing/LoginScreen.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.awt.Color;
 import java.awt.Cursor;
-
+import java.awt.Font;  
 
 class LoginScreen extends JFrame
 {
@@ -18,20 +18,36 @@ LoginScreen()
 }
 void setComponents()
 {
-	
-	Cursor c1 = new Cursor(Cursor.HAND_CURSOR);
+
+Font f = new Font("Calibri", Font.BOLD, 25);
+Font lablel1 = new Font("Calibri", Font.BOLD, 25);	
+
+Cursor c1 = new Cursor(Cursor.HAND_CURSOR);
 l1 = new JLabel("Welcome to Java Learning By Saurabhh");
+l1.setFont(lablel1);
 l2 = new JLabel("USERNAME");
+l2.setFont(f);
 l3 = new JLabel("PASSWORD");
+l3.setFont(f);
 l4 = new JLabel();
 t1 = new JTextField();
 t2 = new JPasswordField();
 b1 = new JButton("Login");
+b1.setBackground(Color.BLUE);
+b1.setForeground(Color.WHITE);
 b1.setCursor(c1);
-
 b2 = new JButton("Clear");
+b2.setBackground(Color.GREEN);
+b2.setForeground(Color.WHITE);
+b2.setCursor(c1);
 b3 = new JButton("Add");
+b3.setBackground(Color.YELLOW);
+b3.setForeground(Color.BLACK);
+b3.setCursor(c1);
 b4 = new JButton("Autofill");
+b4.setBackground(Color.PINK);
+b4.setForeground(Color.WHITE);
+b4.setCursor(c1);
 setLayout(null);
 add(l1);
 add(l2);
@@ -43,20 +59,22 @@ add(b1);
 add(b2);
 add(b3);
 add(b4);
-l1.setBounds(100, 50, 300, 30);
-l2.setBounds(100, 200, 100, 30);
-l3.setBounds(100, 350, 100, 30);
+l1.setBounds(130, 50, 450, 40);
+l2.setBounds(150, 150, 200, 50);
+l3.setBounds(150, 250, 200, 50);
 l4.setBounds(100, 550, 200, 30);
-t1.setBounds(350, 200, 100, 30);
-t2.setBounds(350, 350, 100, 30);
-b1.setBounds(200, 450, 100, 30);
-b2.setBounds(400, 450, 100, 30);
-b3.setBounds(500, 450, 100, 30);
-b4.setBounds(300, 450, 100, 30);
+t1.setBounds(350, 150, 100, 30);
+t2.setBounds(350, 250, 100, 30);
+b1.setBounds(130, 350, 100, 30);
+b2.setBounds(230, 350, 100, 30);
+b3.setBounds(330, 350, 100, 30);
+b4.setBounds(430, 350, 100, 30);
 b1.addActionListener(new LoginButton());
 b2.addActionListener(new ClearButton());
 b3.addActionListener(new Add());
 b4.addActionListener(new Autofill());
+l2.addMouseListener(new MouseL());
+
 }
 public static void main(String [] args)
 {
@@ -66,6 +84,30 @@ s1.setSize(700,700);
 s1.setComponents();
 s1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
+
+class MouseL  implements MouseListener
+{
+	
+	public void mouseClicked(MouseEvent e) {  
+        l2.setText("Mouse Clicked");  
+    }  
+    public void mouseEntered(MouseEvent e) {  
+        l2.setText("Mouse Entered");  
+    }  
+    public void mouseExited(MouseEvent e) {  
+        l2.setText("USERNAME");  
+    }  
+    public void mousePressed(MouseEvent e) {  
+        l2.setText("Mouse Pressed");  
+    }  
+    public void mouseReleased(MouseEvent e) {  
+        l2.setText("Mouse Released");  
+    }  
+	
+}
+
+
+
 class LoginButton implements ActionListener
 {
 public void actionPerformed(ActionEvent e1)	
