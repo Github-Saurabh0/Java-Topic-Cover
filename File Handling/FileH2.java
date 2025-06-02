661ba763f60dca16329dd348ea0e7ff65c07ceca
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-02 17:22:56 +0530

    Create File, Store data using Output Stream File handling

--- File Handling/FileH2.java
+++ File Handling/FileH2.java
@@ -7,9 +7,11 @@ public static void main(String [] args) throws IOException
 
 File f1 = new File("ChottuH2.txt");
 f1.createNewFile();
+System.out.println("File Created "+f1);
 
 String s1 = "Saurabhh By Java";
 FileOutputStream fout = new FileOutputStream("ChottuH2.txt");
+
 
 char c1[] = s1.toCharArray();
 int i;
@@ -17,6 +19,9 @@ for(i=0; i<c1.length; i++)
 {
 	fout.write(c1[i]);
 }
+System.out.println(f1.exists());
+System.out.println(f1.canWrite());
+System.out.println(f1.length());
 fout.close();
 
 }
