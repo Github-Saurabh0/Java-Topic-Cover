4516f17a0486d023a705f5c4e5e28aa7de56ce9d
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-02 14:41:21 +0530

    Create File using File handling and cheack perameters

---
+++ File Handling/FileH1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class FileH1
+{
+public static void main(String [] args) throws IOException
+{
+
+File f1 = new File("Chottu.txt");
+
+System.out.println(f1.exists());
+System.out.println(f1.canWrite());
+System.out.println(f1.length());
+
+// f1.createNewFile();
+
+// System.out.println(f1.exists());
+// System.out.println(f1.canWrite());
+// System.out.println(f1.length());
+
+}
+
+}
